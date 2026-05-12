3e94f0271d5bf85bc12d7a8aadd8012586aedc3a
Wojciech <wenter2005pl@gmail.com>
2026-05-12 14:31:18 +0200

    changesIDK

--- src/main/java/org/example/services/hibernateService/impl/RentalHibernateService.java
+++ src/main/java/org/example/services/hibernateService/impl/RentalHibernateService.java
@@ -1,15 +1,49 @@
 package org.example.services.hibernateService.impl;
 
+import org.example.db.HibernateConfig;
 import org.example.models.Rental;
+import org.example.models.User;
+import org.example.models.Vehicle;
+import org.example.repositories.RentalRepository;
 import org.example.services.hibernateService.RentalServiceInterface;
+import org.hibernate.Session;
+import org.hibernate.Transaction;
 
 import java.util.List;
 import java.util.Optional;
 
 public class RentalHibernateService implements RentalServiceInterface {
+    private final RentalRepository rentalRepository;
+    private final VehicleHibernateService vehicleRepository;
+    private final UserHibernateService userRepository;
+
+    public RentalHibernateService(RentalRepository rentalRepository, VehicleHibernateService vehicleHibernateService, UserHibernateService userHibernateService) {
+        this.rentalRepository = rentalRepository;
+        this.vehicleRepository = vehicleHibernateService;
+        this.userRepository = userHibernateService;
+    }
+
+
     @Override
     public Rental rentVehicle(String userId, String vehicleId) {
-        return null;
+        Transaction tx = null;
+        try(Session session = HibernateConfig.getSessionFactory().openSession()) {
+            tx = session.beginTransaction();
+
+            setSession(session);
+
+            boolean userHasActivRental = rentalRepository.findAll().stream()
+                    .anyMatch(r->userId.equals(r.getUserId())&& r.isActive());
+            if(userHasActivRental){
+                throw new IllegalStateException("Masz już aktywne wypożyczenie");
+            }
+            Vehicle vehicle = vehicleRepository.findById(vehicleId)
+                    .orElseThrow(()-> new IllegalArgumentException("Nie znaleziono pojazdu"));
+            User user = userRepository.findById(userId).orElseThrow(()-> new IllegalArgumentException(ytkownika))
+        }
+    }
+
+    private void setSession(Session session) {
     }
 
     @Override
